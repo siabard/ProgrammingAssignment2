1f7c1b6731f70d9799d8f830405b59859aaab9f4
Ian Jang <siabard@gmail.com>
2016-03-17 07:50:36 +0900

    * implement cacheSolve * implement makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2
+## R-Programming
+## Original Author :  Roger D. Peng <rdpeng@gmail.com>
+## Forked By : Yeonho Jang <siabard@gmail.com>
 
-## Write a short comment describing this function
+## creates a special vector containing functions
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  # set the value of the vector
+  inv <- NULL
+  src <- x
+  set <- function( mA ) {
+    src <<- mA
+    inv <- NULL
+  }
+  # get the value of the matrix
+  
+  get <- function() src
+  
+  # set the value of the invert of given matrix
+  
+  setInv <- function( mInv ) inv <<- mInv
+  
+  # get the value of the invert of given matrix
+  getInv <- function() inv
+  
+  list(set=set,
+       get=get,
+       setInv=setInv,
+       getInv=getInv)
 }
 
 
@@ -12,4 +35,23 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setInv(inv)
+  inv
 }
+
+## TEST case
+# A = matrix(c(1, 2, 3, 0, 1, 4, 5, 6, 0) , nrow=3, ncol=3, byrow=TRUE)
+# invA = solve(A)
+# cA = makeCacheMatrix(A)
+# cInvA = cacheSolve(A)
+
+## should be all true matrix
+# invA == cA$getInv()
